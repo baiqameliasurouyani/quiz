8001005bb6ceb1a23612e49f39d2baf9461d4dbd
baiqameliasurouyani <129942397+baiqameliasurouyani@users.noreply.github.com>
2023-04-19 22:26:24 +0800

    Add files via upload

---
+++ quiz dictionary.py
@@ -0,0 +1,80 @@
+biodata = {
+"nama":"linda",
+"nim":"19220001",
+"fakultas":"ilmu komputer",
+"jurusan":"sistem informasi",
+"alamat":"jawa",
+"matakuliah":"kalkulus,sistem basis data,ddp",
+"nilai":"75,80,85"
+}
+print("nama kamu adalah = " + biodata["nama"])
+print("nim kamu adalah =" , biodata["nim"])
+print("fakultas kamu adalah = " + biodata["fakultas"])
+print("jurusan kamu adalah = " + biodata["jurusan"])
+print("alamat kamu adalah =" + biodata["alamat"])
+print("matakuliah kamu adalah =" + biodata["matakuliah"])
+print("nilai kamu adalah = " + (biodata.get("nilai")))
+biodata = {
+    "nama":"kudus",
+    "nim":"19220022",
+    "fakultas":"ilmu komputer",
+    "jurusan":"sistem informasi",
+    "alamat":"bali",
+    "matakuliah":"PAI,pendidikan pancasila,sistem informasi manajemen",
+    "nilai":"80,75,70"
+}
+print("nama adalah = " + biodata["nama"])
+print("nim adalah = " , biodata ["nim"])
+print("fakultas adalah = " + biodata["fakultas"])
+print("jurusan adalah = " + biodata["jurusan"])
+print("alamat adalah = " + biodata["alamat"])
+print("matakuliah adalah = " + biodata["matakuliah"])
+print ("nilai adalah = " + (biodata.get("nilai")))
+biodata = {
+    "nama":"fitri",
+    "nim":"19220004",
+    "fakultas":"kedokteran",
+    "jurusan":"dokter gigi",
+    "alamat":"bima",
+    "matakuliah":"kesehatan gigi mulut,penyakit jaringan gigi,ilmu kedokteran gigi anak",
+    "nilai":"70,75,90"
+}
+print("nama anda = " + biodata["nama"])
+print("nim anda = " , biodata["nim"])
+print("fakultas anda = " + biodata["fakultas"])
+print("jurusan anda = " + biodata["jurusan"])
+print("alamat anda = " + biodata["alamat"])
+print("matakuliah anda=" + biodata["matakuliah"])
+print("nilai anda=" + (biodata.get("nilai")))
+biodata={
+    "nama":"jamal",
+    "nim":"192220025",
+    "fakultas":"pendidikan",
+    "jurusan":"PGSD",
+    "alamat":"lombok timur",
+    "matakuliah":"konsep dasar ipa,konsep dasar ips,konsep dasar matematika",
+    "nilai":"75,85,90"
+}
+print("nama kamu adalah = " + biodata["nama"])
+print("nim kamu adalah = " , biodata["nim"])
+print("fakultas kamu adalah =" + biodata["fakultas"])
+print("jurusan kamu adalah =" + biodata["jurusan"])
+print("alamat kamu adalah =" + biodata["alamat"])
+print("matakuliah kamu adalah = " + biodata["matakuliah"])
+print("nilai kamu adalah = " + (biodata.get("nilai")))
+biodata = {
+    "nama":"salsa",
+    "nim":"19220034",
+    "fakultas":"pendidikan",
+    "jurusan":"matematika",
+    "alamat":"kopang",
+    "matakuliah":"aljabar linear,aljabar matriks,analisis numerik",
+    "nilai":"70,80,85"
+}
+print("nama adalah = " + biodata["nama"])
+print("nim adalah = " , biodata["nim"])
+print("fakultas adalah=" + biodata["fakultas"])
+print("jurusan adalah=" + biodata["jurusan"])
+print("alamat adalah=" + biodata["alamat"])
+print("matakuliah adalah = " + biodata["matakuliah"])
+print("nilai adalah = " + (biodata.get("nilai")))
